c9f04afcc1a512585673bfcf8edf2bb4ff3d347e
Ebi <me.adibzadeh@gmail.com>
2022-06-30 04:16:45 +0430

    Complete vfh & steering

--- src/vfh.py
+++ src/vfh.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from copy import copy, deepcopy
+import math
+import tf
 from math import inf
 import rospy
 import matplotlib.pyplot as plt
@@ -33,8 +35,12 @@ class Controller:
         self.VFH_A = rospy.get_param("/vfh/vfh_a")
         self.VFH_B = rospy.get_param("/vfh/vfh_b")
         
-        self.goal_point = (goal_x, goal_y)
+        self.GOAL_POINT = (goal_x, goal_y)
 
+        self.cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
+        self.k_p_teta = 0.7
+        self.odom_msg = None
+        
 
     def get_laser_ranges(self):        
         rospy.wait_for_service('/get_distance')
@@ -55,7 +61,7 @@ class Controller:
             print("Service call failed: %s"%e)
          
     def find_valleys(self, hk_prime):
-        print(hk_prime)
+        # print(hk_prime)
         valleys_idx = []
         for i in range(len(hk_prime)):
             if hk_prime[i] < self.VFH_THRESHOLD:
@@ -67,7 +73,7 @@ class Controller:
         i = 0
         while i < len(valleys_idx):
             while i < len(valleys_idx) and valleys_idx[i] == (valleys_idx[i-1])%len(hk_prime) + 1:
-                print(f"####{i} {len(valleys_idx)}")
+                # print(f"####{i} {len(valleys_idx)}")
                 vall.append(valleys_idx[i])
                 i += 1
             
@@ -87,17 +93,87 @@ class Controller:
                 valleys[0] = val_begin
                 valleys.pop(len(valleys)-1)
             
-        print(f":)))))))))))))))))|| {valleys}")
+        # print(f":)))))))))))))))))|| {valleys}")
             
         # print(f"@@@@@@@@@@@@@@@@@@@@{hk_prime}")
         # print(f"@@@@@@@@@@@@@@@@@@@@{valleys}")
         # print(f"@@@@@@@@@@@@@@@@@@@@{type(valleys)}")
-        print("---------------------------------------------------------------------------")
+        # print("---------------------------------------------------------------------------")
         return valleys
        
-       
+    
+    
+    def get_odom_msg(self):
+        self.odom_msg = rospy.wait_for_message("/odom", Odometry)
+        # print(f'akhe gozoooo: {self.odom_msg.pose.pose.orientation}')
+
+
+    def calculate_angle_diff(self, point):
+        roll, pitch, yaw = tf.transformations.euler_from_quaternion((
+            self.odom_msg.pose.pose.orientation.x,
+            self.odom_msg.pose.pose.orientation.y,
+            self.odom_msg.pose.pose.orientation.z,
+            self.odom_msg.pose.pose.orientation.w
+        ))
+        # print(f"YAW: {yaw}")
+
+        point_x, point_y = point
+        # rospy.loginfo(f"$$$$${point}|{point_x}|{point_y}")
+        if point_x - self.odom_msg.pose.pose.position.x == 0:
+            if point_y > self.odom_msg.pose.pose.position.y:
+                angle_to_point = math.pi/2
+            else:
+                angle_to_point = 3*math.pi/2
+        else:
+            angle_to_point = math.atan2(point_y - self.odom_msg.pose.pose.position.y, point_x - self.odom_msg.pose.pose.position.x)
+        
+        angle_to_point = (2*math.pi + angle_to_point) % (2*math.pi)
+        yaw = (2*math.pi + yaw) % (2*math.pi)
+        # print(f"koskhol: {self.odom_msg.pose.pose.position} {point}")
+        
+        if angle_to_point > 0.9* 2 * math.pi and yaw < 0.1*math.pi:
+            yaw += 2*math.pi
+        diff_angle = angle_to_point - yaw
+        if diff_angle > math.pi:
+            diff_angle = 2*math.pi - diff_angle
+        elif diff_angle < -math.pi:
+            diff_angle = 2*math.pi + diff_angle
+        
+        # rospy.loginfo(f"\n====================================================\nangle_to_point: {math.degrees(angle_to_point)}\nyaw:            {math.degrees(yaw)}\ndiff:          {math.degrees(diff_angle)}")
+        return diff_angle
+
+ 
     def find_steering_direction(self, valleys):
-        pass
+        self.get_odom_msg()
+        angle_diff = self.calculate_angle_diff(self.GOAL_POINT)
+        angle_diff = math.degrees(angle_diff)
+        
+        angle_diff/=5        
+        best_valley = None
+        min_diff = 100000
+        kn = None
+        for valley in valleys:
+            if int(angle_diff) in valley:
+                print(f"oskol2222: angle diff/5:{angle_diff * 5} \n{valley} ")
+                return math.radians(angle_diff * 5)
+             
+            for i in range(len(valley)):
+                a = abs(angle_diff - valley[i])
+                if a < min_diff:
+                    min_diff = a
+                    best_valley = valley
+                    kn = valley[i]
+        
+        kf = (kn + self.VFH_S_MAX) % (360/5)
+        if len(best_valley) > self.VFH_S_MAX:
+            teta = (kn+kf)/2 * 5
+            teta %= 360
+        else:
+            teta = (best_valley[0] + best_valley[len(best_valley)-1]) / 2 * 5
+            teta %= 360
+            
+        print(f"oskol: sterring:{teta} angle diff:{angle_diff * 5} | kn: {kn} kf: {kf}\n{best_valley} ")
+        return math.radians(teta)
     
 
     def calculate_vfh(self, laser_ranges):
@@ -118,18 +194,34 @@ class Controller:
             # rospy.loginfo(v)
             hk_prime.append(sum(v)/len(v))
         
+        
         valleys = self.find_valleys(hk_prime)        
-        self.find_steering_direction(valleys)         
+        teta = self.find_steering_direction(valleys)         
+
+        # print(f'laser_range: {laser_ranges}')
+        print(f'hk_orime: {hk_prime}')        
+        print(f'valleys: {valleys}')        
+        return teta
 
         
     def navigate_to_point(self):
+            # rospy.loginfo(ranges)
         while True:
             ranges = self.get_laser_ranges()
-            # rospy.loginfo(ranges)
             if ranges != None:
-                self.calculate_vfh(ranges)
+                teta = self.calculate_vfh(ranges)
+                self.move(teta)
 
     
+    def move(self, teta): 
+        print(f"*********************************\n{teta}\n*******************************")
+        move_cmd = Twist()
+        # move_cmd.angular.z = 0
+        move_cmd.linear.x = 0.3
+
+        P_TETA = self.k_p_teta * teta
+        move_cmd.angular.z = P_TETA 
+        self.cmd_publisher.publish(move_cmd)
 
     
 if __name__ == "__main__":
