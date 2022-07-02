d4aeddff5f8a42e56920579e63975fb9f9a20102
tanya-jp <t_djavaherpour@yahoo.com>
2022-07-02 03:26:25 -0700

    Add dijkstra

--- src/shortest_distacnce_calculator.py
+++ src/shortest_distacnce_calculator.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import yaml
 from math import sqrt
+from queue import PriorityQueue
 from cv_bridge import CvBridge
 import rospy
 from geometry_msgs.msg import Twist
@@ -11,6 +12,43 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from sensor_msgs.msg import Image
+
+class Graph:
+    def __init__(self, num_of_vertices):
+        self.v = num_of_vertices
+        self.edges = [[-1 for i in range(num_of_vertices)] for j in range(num_of_vertices)]
+        self.visited = []
+    
+    def add_edge(self, u, v, weight):
+        self.edges[u][v] = weight
+        self.edges[v][u] = weight
+    
+    def dijkstra(graph, start_vertex, res):
+      D = {v:float('inf') for v in range(graph.v)}
+      # print(D)
+      D[start_vertex] = 0
+
+      pq = PriorityQueue()
+      pq.put((0, start_vertex))
+      
+  
+      while not pq.empty():
+          (dist, current_vertex) = pq.get()
+          graph.visited.append(current_vertex)
+
+          for neighbor in range(graph.v):
+              if graph.edges[current_vertex][neighbor] != -1:
+                  distance = graph.edges[current_vertex][neighbor]
+                  if neighbor not in graph.visited:
+                      old_cost = D[neighbor]
+                      new_cost = D[current_vertex] + distance
+                      if new_cost < old_cost:
+                          pq.put((new_cost, neighbor))
+                          res[neighbor] = current_vertex
+                          D[neighbor] = new_cost
+      
+      return D, res
+
 
 class ReadMap():
     def __init__(self) -> None:
@@ -25,6 +63,8 @@ class ReadMap():
         self.height = self.pgm_file.shape[0]
         self.width = self.pgm_file.shape[1]
         self.free_cells = np.zeros([self.height, self.width])
+        self.nodes = {}
+        self.pixel_nodes_num = {}
 
     def get_pix_value(self):
         for i in range(self.height):
@@ -59,10 +99,12 @@ class ReadMap():
         j = start_y
         while(self.free_cells[start_x][j] >0 and self.height-1>j>0):
             j +=1
-            flag +=1
-        end_point = [start_x, j-1]
-        d = abs(j - start_y)
-        return end_point, d
+            # flag +=1
+        mian_d = abs(j - start_y)
+        y = start_y+mian_d//2
+        end_point = [start_x, y]
+        d = abs(y - start_y)
+        return end_point, d, mian_d
     
     def go_down(self, start_x, start_y):
         j = start_y
@@ -78,9 +120,12 @@ class ReadMap():
         i = start_x
         while(self.free_cells[i][start_y] >0 and self.width-1>i>0):
             i +=1
-        end_point = [i-1, start_y]
-        d = abs(i - start_x)
-        return end_point, d
+
+        mian_d = abs(i - start_x)
+        x = start_x+mian_d//2
+        end_point = [x, start_y]
+        d = abs(x - start_x)
+        return end_point, d, mian_d
     
     def go_left(self, start_x, start_y):
         i = start_x
@@ -93,36 +138,93 @@ class ReadMap():
         return end_point, d
 
     def create_graph(self, start_x_pixel, start_y_pixel):
-        graph={}
+
+        # horizontal_graph=[]
+        # vertical_graph = []
+        graph = []
+        node_num = 0
         for i in range(0, self.width-1):
             j = 0
             while(j < self.height):
                 start_point = [i, j]
-                end_point_u, d_u = self.go_up(i, j)
+                end_point_u, d_u, mian_d = self.go_up(i, j)
                 if d_u > 0:
-                    key_u = [start_point, end_point_u]
-                    graph[str(key_u)] = d_u
-                    j += d_u
+                    d = np.zeros(5)
+                    # key_u = [start_point, end_point_u]
+                    # graph[str(key_u)] = d_u
+                    d[0] = i
+                    d[1] = j
+                    d[2] = end_point_u[0]
+                    d[3] = end_point_u[1]
+                    d[4] = d_u
+                    graph.append(d)
+                    rospy.loginfo(f"%%%%%%%%%%% : {d}")
+                    j += mian_d
+                    # start_str = str(str(i)+" "+str(j))
+                    # end_str = str(str(end_point_u[0])+ " "+str(end_point_u[1]))
+                    self.nodes[node_num] = start_point
+                    self.nodes[node_num + 1] = end_point_u
+                    self.pixel_nodes_num[str(start_point)] = node_num
+                    self.pixel_nodes_num[str(end_point_u)] = node_num + 1
+                    # end_point_r, d_r = self.go_right(d[2], d[3])
+                    node_num += 2
+
+
                 else:
                     j +=1
-                # rospy.loginfo(f"%%%%%%%%%%% : {d_u}   {i} {j}")
+                
 
 
         for j in range(0, self.height-1):
             i = 0
             while(i < self.width):
                 start_point = [i, j]
-                end_point_r, d_r = self.go_right(i, j)
+                end_point_r, d_r, mian_d = self.go_right(i, j)
                 if d_r > 0:
-                    key_r = [start_point, end_point_r]
-                    graph[str(key_r)] = d_r
-                    i += d_r
+                    d = np.zeros(5)
+                    # key_r = [start_point, end_point_r]
+                    # graph[str(key_r)] = d_r
+                    d[0] = i
+                    d[1] = j
+                    d[2] = end_point_r[0]
+                    d[3] = end_point_r[1]
+                    d[4] = d_r
+                    graph.append(d)
+                    rospy.loginfo(f"^^^^^^^ : {d}")
+                    i += mian_d
+
+                    # start_str = str(str(i)+" "+str(j))
+                    # end_str = str(str(end_point_r[0])+ " "+str(end_point_r[1]))
+                    if not str(start_point) in self.pixel_nodes_num:
+                        self.nodes[node_num] = start_point
+                        self.pixel_nodes_num[str(start_point)] = node_num
+                        node_num += 1 
+                    if not str(end_point_r) in self.pixel_nodes_num:
+                        self.nodes[node_num] = end_point_r
+                        self.pixel_nodes_num[str(end_point_r)] = node_num
+
+                        node_num += 1 
                 else:
                     i+=1
-                # rospy.loginfo(f"^^^^^^^ : {d_r}   {i} {j}")
+                
+        # rospy.loginfo(f"^^^^^^^^^^ : {self.pixel_nodes_num}")
+        g = Graph(node_num)
+        rospy.loginfo(f"%%%%%%%%%%% : {50000000000}")
+        for i in range(len(graph)):
+            start_x = int(graph[i][0])
+            start_y = int(graph[i][1])
+            end_x = int(graph[i][2])
+            end_y = int(graph[i][3])
+            cost = graph[i][4]
+            start = [start_x, start_y]
+            end = [end_x, end_y]
+            start_node_num = int(self.pixel_nodes_num[str(start)])
+            end_node_num = int(self.pixel_nodes_num[str(end)])
+            g.add_edge(start_node_num, end_node_num, cost)
+        
+        D, pq = g.dijkstra(0, {})
+        rospy.loginfo(f"&&&&&&& : {pq}")
         return graph
-
-
 
 
 
@@ -138,8 +240,10 @@ class Controller():
         self.cell_value = f.get_pix_value()
         self.x_goal_pixel, self.y_goal_pixel = f.get_goal_pixel(self.goal_x, self.goal_y)
         self.x_robot_pixel, self.y_robot_pixel = f.get_robot_pixel(self.start_x, self.start_y)
+        self.graph = []
         self.graph = f.create_graph(self.x_robot_pixel, self.y_robot_pixel)
         # rospy.loginfo(f"%%%%%%%%%%% : {self.graph}")
+        # rospy.loginfo(f"^^^^^^^^^^ : {len(self.graph)}")
     def run(self):
         # print("(((((((((((((((((((((((((")
         while(1):
